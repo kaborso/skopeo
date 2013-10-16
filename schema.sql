fff09faa47c8d09905b9e7596d4711c02b0a2674
netcrusher88 <netcrusher88@gmail.com>
2013-10-15 21:44:24 -0700

    Added whitespace for clearer schema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,19 @@
 CREATE TABLE games (id INTEGER PRIMARY KEY, name TEXT);
+
 CREATE TABLE periods (id INTEGER PRIMARY KEY, game_id REFERENCES games.id,
     content TEXT, next INTEGER, previous INTEGER, tone TEXT);
+    
 CREATE TABLE events (id INTEGER PRIMARY KEY, period_id REFERENCES periods.id,
     content TEXT, next INTEGER, previous INTEGER, tone TEXT);
+    
 CREATE TABLE scenes (id INTEGER PRIMARY KEY, event_id REFERENCES events.id, 
     question TEXT, setting TEXT, answer TEXT, 
     next INTEGER, previous INTEGER, tone TEXT);
 
 CREATE INDEX period_gameid ON periods (game_id);
+
 CREATE INDEX event_periodid ON events (period_id);
+
 CREATE INDEX scene_eventid ON scenes (event_id);
 
 CREATE TRIGGER period_insert AFTER INSERT ON periods BEGIN 
@@ -16,11 +21,13 @@ CREATE TRIGGER period_insert AFTER INSERT ON periods BEGIN
     UPDATE periods SET previous = new.previous WHERE previous IS new.previous
         AND id != new.id;
 END;
+
 CREATE TRIGGER event_insert AFTER INSERT ON events BEGIN 
     UPDATE events SET next = new.id WHERE next IS new.next AND id != new.id;
     UPDATE events SET previous = new.previous WHERE previous IS new.previous
         AND id != new.id;
 END;
+
 CREATE TRIGGER scene_insert AFTER INSERT ON scenes BEGIN 
     UPDATE scenes SET next = new.id WHERE next IS new.next AND id != new.id;
     UPDATE scenes SET previous = new.previous WHERE previous IS new.previous
